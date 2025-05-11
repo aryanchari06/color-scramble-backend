c69b9be667809f1e574505a2018222a8a64d8cbc
aryanchari06 <aryaney0704@gmail.com>
2025-05-11 14:35:21 +0530

    wrongCount update

--- app.js
+++ app.js
@@ -9,27 +9,29 @@ export const startWs = (server) => {
     },
   });
 
-  let wrongCount = 0;
   io.on("connection", (socket) => {
     console.log(socket.id);
-    // socket.emit("new-user", { message: `New user connected ${socket.id}` });
 
     socket.on("join-room", (roomId) => {
       let scoreYou = 0;
-
       socket.join(roomId);
-      // const clientsInRoom = io.sockets.(roomId);
+
       io.to(roomId).emit("new-user", {
         message: `User ${socket.id} has joined the room ${roomId}`,
       });
 
       const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+      let wrongCount = io.sockets.adapter.rooms.get(roomId)?.wrongCount || 0;
 
       console.log("Clients in the room: ", clients);
 
+      // Initialize wrongCount for the room if it doesn't exist
+      if (!io.sockets.adapter.rooms.get(roomId)?.wrongCount) {
+        io.sockets.adapter.rooms.get(roomId).wrongCount = 0;
+      }
+
       if (clients.length >= 2) {
         const { randomColorName, shuffledObj } = selectColors();
-        // console.log(randomColorName)
         io.to(roomId).emit("game-data", { randomColorName, shuffledObj });
       }
 
@@ -43,19 +45,20 @@ export const startWs = (server) => {
         });
 
         const { randomColorName, shuffledObj } = selectColors();
-        // console.log(randomColorName)
         io.to(roomId).emit("game-data", { randomColorName, shuffledObj });
-        wrongCount = 0
+        wrongCount = 0; // reset the wrong count when someone answers correctly
       });
 
       socket.on("wrong-answer", () => {
-        wrongCount++;
+        // Increment the wrongCount for this room
+        io.sockets.adapter.rooms.get(roomId).wrongCount++;
+        wrongCount = io.sockets.adapter.rooms.get(roomId).wrongCount;
+
         console.log(wrongCount);
         if (wrongCount === clients.length) {
-          // console.log("get new gamedata")
           const { randomColorName, shuffledObj } = selectColors();
           io.to(roomId).emit("game-data", { randomColorName, shuffledObj });
-          wrongCount = 0;
+          io.sockets.adapter.rooms.get(roomId).wrongCount = 0; // reset wrongCount for the room
         }
       });
 
@@ -66,16 +69,13 @@ export const startWs = (server) => {
       });
 
       socket.on('disconnect', () => {
-        socket.leave(roomId)
-        console.log('Clients in my room', clients)
-      })
+        socket.leave(roomId);
+        console.log('Clients in my room', clients);
+      });
     });
+
     socket.on("message", (msg) => {
       console.log(msg);
     });
-
-    // socket.on("disconnect", () => {
-    //   socket.leave(roomId)
-    // })
   });
 };
